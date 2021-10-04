3b4099c6d1f32320315fa93918e0f8e0de498c56
QuickCloudLearner <ali.uib@gmail.com>
2021-10-04 11:51:17 +0200

    added additional terraform files

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+#-------------------------------
+# Provide main logic here
+#-------------------------------
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.resource_group_location
